1ed26bfb0c44d51bc9b05178b43b16c07b28088a
DilmurodjonT <dilmurodjontadjibaev@gmail.com>
2023-06-26 16:03:50 +0500

    start Form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Form } from "./components/form/Form";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello word</h1>
+      <Form />
     </div>
   );
 }
